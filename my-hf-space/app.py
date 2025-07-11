f5f08f51b45d304d0b57e5603acc39ef63365b21
Mashroor Newaz <mashroornewaz37@gmail.com>
2025-07-10 22:38:21 -0400

    Bug fixes

--- my-hf-space/app.py
+++ my-hf-space/app.py
@@ -3,32 +3,38 @@ import joblib
 import numpy as np
 import pandas as pd
 
-model = joblib.load("model.joblib")
+DoorDash_data = pd.read_csv("../datasets/historical_data.csv")
+market_ids = DoorDash_data["market_id"].unique().tolist()
+order_protocols = DoorDash_data["order_protocol"].unique().tolist()
+store_categories = DoorDash_data["store_primary_category"].unique().tolist()
 
-def predict(categoryindian, market_id_1, order_protocol_3,total_busy_dashers,
+#Load the model
+model = joblib.load("model.joblib")
+model_columns = joblib.load("model_columns.pkl")
+
+def predict(store_category, market_id, order_protocol,total_busy_dashers,
             total_onshift_dashers, total_outstanding_orders,avg_price_per_item, estimated_drive_duration):
     
-    
     busy_dashers_ratio = total_busy_dashers/total_onshift_dashers
+    store_category_col = "category" + store_category
+    market_id_col = f"market_id_{market_id}.0"
+    order_protocol_col = f"order_protocol_{order_protocol}.0"
 
-    X_input = pd.DataFrame([[
-        categoryindian,
-        market_id_1,
-        order_protocol_3,
-        busy_dashers_ratio,
-        total_outstanding_orders,
-        avg_price_per_item,
-        estimated_drive_duration
-    ]], columns=[
-        "categoryindian",
-        "market_id_1.0",
-        "order_protocol_3.0",
-        "busy_dashers_ratio",
-        "total_outstanding_orders",
-        "avg_price_per_item",
-        "estimated_store_to_consumer_driving_duration"
-    ])
-    
+    X_input = pd.DataFrame([[0]*len(model_columns)], columns=model_columns)
+
+    # Fill in selected fields
+    if store_category_col in X_input.columns:
+        X_input.at[0, store_category_col] = 1
+    if market_id_col in X_input.columns:
+        X_input.at[0, market_id_col] = 1
+    if order_protocol_col in X_input.columns:
+        X_input.at[0, order_protocol_col] = 1
+
+    X_input.at[0, "busy_dashers_ratio"] = busy_dashers_ratio
+    X_input.at[0, "total_outstanding_orders"] = total_outstanding_orders
+    X_input.at[0, "avg_price_per_item"] = avg_price_per_item
+    X_input.at[0, "estimated_store_to_consumer_driving_duration"] = estimated_drive_duration
+
 
     prediction = model.predict(X_input)
     return str(prediction[0])
@@ -36,9 +42,9 @@ def predict(categoryindian, market_id_1, order_protocol_3,total_busy_dashers,
 demo = gr.Interface(
     fn= predict,
     inputs=[
-        gr.Radio([0,1],label="Is Indian category"),
-        gr.Radio([0,1],label="Is market id 1.0"),
-        gr.Radio([0,1],label="Is order protocol 3.0"),
+        gr.Dropdown(store_categories, label="Store Category"),
+        gr.Dropdown(market_ids, label="Market ID"),
+        gr.Dropdown(order_protocols, label="Order Protocol"),
         gr.Number(label="Number of busy dashers"),
         gr.Number(label="Number of total onshift dashers"),
         gr.Number(label="Total Outstanding Orders"),
